dc4b9159844acfa41ded1a4919487d5593171a20
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-02-07 20:35:01 +0000

    formatting only

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,19 +1,18 @@
- let maze: Maze; 
+let maze: Maze;
 function setup() {
-    const l = min(windowHeight, windowWidth)
-    createCanvas(l, l);
-    noLoop();
-    maze = createMaze(5);
-    console.log(maze)
+  const l = min(windowHeight, windowWidth);
+  createCanvas(l, l);
+  noLoop();
+  maze = createMaze(5);
+  console.log(maze);
 }
 
 function draw() {
-    background("beige");
-    drawMaze(maze, 80)
+  background("beige");
+  drawMaze(maze, 80);
 }
-
 
 // If user clicks, draw() will be called again (indirectly)
 function mousePressed() {
-    redraw();
+  redraw();
 }
